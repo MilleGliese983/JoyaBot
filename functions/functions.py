67a73275b776fe808812be4a68b8d26bc7aeb28f
MilleGliese983 <88241983mille@gmail.com>
2025-12-31 11:12:05 +0900

    first commit

---
+++ functions/functions.py
@@ -0,0 +1,44 @@
+from mastodon import Mastodon
+from mastodon.return_types import Status
+from bs4 import BeautifulSoup
+from datetime import datetime, timezone, timedelta
+import re
+import random
+
+def get_name(account: User):
+    if len(account['display_name']) == 0:
+        return account['username']
+    else:
+        return account['display_name']
+
+def rewrite(text: str, doRet: bool = True):
+    if doRet:
+        text = text.replace('</p><p>', '\n\n')
+        text = text.replace('<br />','\n')
+    else:
+        text = text.replace('</p><p>', ' ')
+        text = text.replace('<br />',' ')
+    text = BeautifulSoup(text, 'html.parser').get_text()
+    text = text.replace('&apos;', '\'')
+    text = text.replace('&amp;', '&')
+    text = text.replace('&quot;', '\"')
+    return text
+
+def deleteBonnou(client: Mastodon, status: Status):
+    client.status_reply(status, "ゴーン")
+
+def countBonnou(client: Mastodon, status: Status):
+    bonnoulist = client.timeline_hashtag("煩悩")
+    yearstart = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    if yearstart.month == 1 and yearstart.day <= 15:
+        yearstart.year -= 1
+    yearstart = yearstart.replace(month=1, day=15)
+    yearend = yearstart.replace(year+=1)
+
+    counter = 0
+    for tl in bonnoulist:
+        toottime = tl['created_at'].replace(tzinfo=None) + timedelta(hours=9)
+    if yearstart <= toottime < yearend:
+        counter += 1
+    
+    client.toot(f"みんなから集まった今年の煩悩は{counter}個です！")
---
+++ functions/streamings.py
@@ -0,0 +1,48 @@
+from mastodon import Mastodon, StreamListener
+from datetime import datetime
+import time
+import re
+from utils.load_env import INSTANCE_URL, ACCESS_TOKEN
+from .functions import get_name, rewrite, deleteBonnou, countBonnou
+
+
+class Bot(StreamListener):
+    def __init__(self, client: Mastodon):
+        super().__init__()
+        self.client = client
+
+    def on_update(self, status: Status):
+        get_status = status
+        get_status['content'] = rewrite(get_status['content'])
+        print(datetime.now())
+        print(f"==={get_name(get_status['account'])}======\n{get_status['content']}\n\n")
+
+        if get_status['account']['bot'] or get_status['reblog'] != None:
+            return
+
+            if "#煩悩" in get_status['content']:
+                deleteBonnou(self.client)
+                return
+            
+            if re.search((r'煩悩.*数'), get_status['content']):
+                countBonnou(self.client)
+                return
+
+
+        
+def login() -> Mastodon:
+    client = Mastodon(
+        access_token = ACCESS_TOKEN,
+        api_base_url = INSTANCE_URL
+    )
+    return client
+
+def LTLlisten(client: Mastodon):
+    bot = Bot(client)
+    while True:
+        try:
+            client.stream_local(bot, timeout=50000)
+        except Exception as e:
+            print(datetime.now())
+            print(e)
+            time.sleep(60)
